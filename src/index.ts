4b9fffd0e59dd16a45d04a31c69b0f23c61ba7aa
Alan Wu <alanwu100@gmail.com>
2023-06-27 12:38:07 -0700

    feat: display each program address and total compute

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import WebSocket, { Server as WebSocketServer } from 'ws';
 import http from 'http';
 import { fetchBlockData } from './solana';
+import { BlockResponse } from '@solana/web3.js';
 
 const app = express();
 const server = http.createServer(app);
@@ -27,6 +28,8 @@ solanaWs.on('message', async (data: WebSocket.Data) => {
     console.log('Received data from Solana:', data);
 
     const blobData = await readBuffer(data);
+
+    //if it's just the confirmation of subscription, move on
     if (!blobData.hasOwnProperty('params')) {
         return;
     }
@@ -37,7 +40,7 @@ solanaWs.on('message', async (data: WebSocket.Data) => {
     if (throttle === 0) {
         try {
             const fetchPromise = fetchBlockData(slot - 10);
-            promiseQueue.push(fetchPromise);
+            promiseQueue.push(fetchPromise); // Add promise to queue
             throttle = 7; // Reset the throttle
         } catch (error) {
             console.error('Error fetching block data:', error);
@@ -54,14 +57,38 @@ const resolvePromises = async () => {
         if (promiseQueue.length > 0) {
             const fetchPromise = promiseQueue.shift();
             try {
-                const fetchedBlock = await fetchPromise;
+                const fetchedBlock : BlockResponse = await fetchPromise;
                 console.log('block:', fetchedBlock);
+                
+                // get a map of program addresses to compute units
+                const computeUnitMap = await processBlock(fetchedBlock);
+                
+                if (computeUnitMap) {
+                    // convert the map into an array of objects
+                    const computeUnitsArray = Array.from(computeUnitMap).map(([programAddress, computeUnits]) => ({
+                        programAddress,
+                        computeUnits
+                    }));
+                    
+                    // Aggregating computeUnits from meta
+                    let totalComputeUnitsMeta = 0;
+                    for (const transaction of fetchedBlock.transactions) {
+                        if (transaction.meta && transaction.meta.computeUnitsConsumed) {
+                            totalComputeUnitsMeta += transaction.meta.computeUnitsConsumed;
+                        }
+                    }
 
-                if (fetchedBlock) {
-                    const fetchedBlockString = JSON.stringify(fetchedBlock);
+                    // create a payload with additional block information
+                    const payload = JSON.stringify({
+                        slot: fetchedBlock.parentSlot + 1, // you might want to use a different property for the slot/block number
+                        computeUnitsMeta: totalComputeUnitsMeta,
+                        programsComputeUnits: computeUnitsArray
+                    });
+                    
+                    // send the JSON string to the frontend
                     wss.clients.forEach(client => {
                         if (client.readyState === WebSocket.OPEN) {
-                            client.send(fetchedBlockString); // Sending as a JSON string
+                            client.send(payload);
                         }
                     });
                 }
@@ -70,11 +97,13 @@ const resolvePromises = async () => {
                 console.error('Error fetching block data:', error);
             }
         }
-        //console.log(promiseQueue.length);
         await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
     }
 };
 resolvePromises();
+
+
+
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
@@ -92,4 +121,46 @@ async function readBuffer(buffer: any) {
     } catch (error) {
         console.error('Error reading Blob data:', error);
     }
+}
+
+async function processBlock(block: BlockResponse) {
+    if (block.transactions.length === 0) {
+        return null;
+    }
+
+    const relevantTransactions = block.transactions.filter(transaction => {
+        //lambda to return transactions with metadata with >0 compute units
+        return transaction.meta
+        && transaction.meta.computeUnitsConsumed 
+        && transaction.meta.computeUnitsConsumed > 0;
+    });
+
+
+    let computeUnitMap: Map<string, number> = new Map<string, number>();
+
+    if (relevantTransactions.length > 0) {
+        for (const transaction of relevantTransactions) {
+            if (transaction.meta && transaction.meta.logMessages) {
+                const logMessages = transaction.meta.logMessages;
+    
+                for (const logMessage of logMessages) {
+                    // regex to parse the log message
+                    const match = logMessage.match(/Program (\S+) consumed (\d+) of \d+ compute units/);
+                    
+                    // if logMessage has expected format, update map
+                    if (match) {
+                        const programAddress = match[1];
+                        const computeUnitsConsumed = parseInt(match[2], 10);
+                        
+                        //if program address already exists in the map, add the compute units, otherwise set it
+                        const currentComputeUnits = computeUnitMap.get(programAddress) || 0;
+                        computeUnitMap.set(programAddress, currentComputeUnits + computeUnitsConsumed);
+                    }
+                }
+            }
+        }
+    }
+    
+    return computeUnitMap;
+    
 }
