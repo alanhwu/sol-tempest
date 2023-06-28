6ca0c23beadc3ec5038d38eb062ee84fced051a6
Alan Wu <alanwu100@gmail.com>
2023-06-28 13:30:23 -0700

    feat: draw associated addresses, new colors

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 * Websocket between frontend and backend
 * Express server for serving static files
 * Backend consists of a Solana client and a websocket server
-* Basic frontend
+* Basic HTML CSS
+* Visualization done with D3.js
--- public/visualization.js
+++ public/visualization.js
@@ -20,26 +20,50 @@ export async function draw(data) {
     const programs = data.programsComputeUnits || [];
 
     // Define linear scale for dot colors
-    const colorScale = d3.scaleLinear()
-        .domain([0, d3.max(programs, d => d.computeUnits)])
-        .range(["#d9d9d9", "#1f78b4"]);
+    // const colorScale = d3.scaleLinear()
+    //     .domain([0, d3.max(programs, d => d.computeUnits)])
+    //     .range(["#d9d9d9", "#1f78b4"]);
+
+    // Define linear scale for lightness based on compute units
+    const lightnessScale = d3.scaleLinear()
+    .domain([0, d3.max(programs, d => d.computeUnits)])
+    .range([80, 40]);
+
+    // Define linear scale for hue based on hash
+    const hueScale = d3.scaleLinear()
+        .domain([0, 0xffffffff])
+        .range([270, 150]); // Hue range mapping to purple-green
 
     // Create tooltips
     const tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
+    // Define zone radius and associated address circle radius
+    const zoneRadius = 40;
+    const associatedAddressRadius = 2;
+
+    // Define offsets to ensure program nodes and associated addresses stay within SVG
+    const xOffset = zoneRadius + associatedAddressRadius;
+    const yOffset = zoneRadius + associatedAddressRadius;
+
+    const linesGroup = svg.append("g");
+
     // Draw dots
     for (const program of programs) {
         const hash = await sha256(program.programAddress);
-        const cx = hashToNumber(hash, 0, 800); // assuming svg width is 800
-        const cy = hashToNumber(hash.slice(8), 0, 600); // assuming svg height is 600
+        const cx = hashToNumber(hash, xOffset, 800 - xOffset); // assuming svg width is 800
+        const cy = hashToNumber(hash.slice(8), yOffset, 600 - yOffset); // assuming svg height is 600
         
+        const mixedHashPart = hash.slice(4, 8) + hash.slice(16, 20);
+        const hue = hueScale(parseInt(mixedHashPart, 16)); //mixing hash to make more entropy
+        const lightness = lightnessScale(program.computeUnits);
+
         const circle = svg.append("circle")
             .attr("cx", cx)
             .attr("cy", cy)
-            .attr("r", 5)
-            .attr("fill", d => colorScale(program.computeUnits))
+            .attr("r", 6) // Slightly larger program address sizes
+            .attr("fill", `hsl(${hue}, 100%, ${lightness}%)`)
             .on("mouseover", event => {
                 tooltip.transition()
                     .duration(200)
@@ -54,15 +78,67 @@ export async function draw(data) {
                     .style("opacity", 0);
             });
 
-        // Case where dot disappears but tooltip remains
-        const observer = new MutationObserver(mutations => {
-            for (const mutation of mutations) {
-                if (mutation.type === 'childList' && !document.body.contains(circle.node())) {
+            drawAssociatedAddresses(linesGroup, circle, program.associatedAddresses, svg, tooltip, zoneRadius);
+
+
+    }
+
+    // Case where dot disappears but tooltip remains
+    const observer = new MutationObserver(mutations => {
+        for (const mutation of mutations) {
+            if (mutation.type === 'childList') {
+                // Check if any circle or line still exists within SVG
+                const circleExists = svg.select("circle").node();
+                const lineExists = svg.select("line").node();
+                if (!circleExists && !lineExists) {
                     tooltip.remove();
                 }
             }
-        });
+        }
+    });
 
-        observer.observe(svg.node(), { childList: true });
+    observer.observe(svg.node(), { childList: true });
+}
+
+async function drawAssociatedAddresses(linesGroup, programCircle, associatedAddresses, svg, tooltip, zoneRadius) {
+    const programCx = +programCircle.attr("cx");
+    const programCy = +programCircle.attr("cy");
+
+    for (const address of associatedAddresses) {
+        const hash = await sha256(address);
+        const angle = hashToNumber(hash, 0, 2 * Math.PI);
+        const radius = hashToNumber(hash.slice(8), 10, zoneRadius); // Increased minimum radius
+
+        const cx = programCx + radius * Math.cos(angle);
+        const cy = programCy + radius * Math.sin(angle);
+
+        // draw a line from the program to the associated address
+        linesGroup.append("line")
+            .attr("x1", programCx)
+            .attr("y1", programCy)
+            .attr("x2", cx)
+            .attr("y2", cy)
+            .attr("stroke", "grey")
+            .attr("stroke-width", 0.5); // Thinner lines
+
+        // draw a small dot for the associated address
+        svg.append("circle")
+            .attr("cx", cx)
+            .attr("cy", cy)
+            .attr("r", 2)
+            .attr("fill", "#999999") // Neutral gray color for associated addresses
+            .on("mouseover", event => {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html(address)
+                    .style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", event => {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
     }
 }
