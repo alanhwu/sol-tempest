a23d1db1cc0fd9344ff2e7671c8af2a289de1e31
Alan Wu <alanwu100@gmail.com>
2023-07-26 18:08:42 -0500

    refactor: rewrite transaction parsing

--- src/blockProcessor.ts
+++ src/blockProcessor.ts
@@ -6,7 +6,7 @@ import { config } from './config';
 export async function processBlock(block: VersionedBlockResponse) {
     //map from Program to CU consumed
     let computeUnitMap: Map<string, number> = new Map<string, number>();
-    let addressToProgramsMap: Map<string, Set<programWithCompute>> = new Map<string, Set<programWithCompute>>();
+    let addressToProgramsMap: Map<string, Set<string>> = new Map<string, Set<string>>();
 
     let accountAddressToComputeMap: Map<string, number> = new Map<string, number>();
 
@@ -15,68 +15,36 @@ export async function processBlock(block: VersionedBlockResponse) {
         transaction.meta.computeUnitsConsumed &&
         transaction.meta.computeUnitsConsumed > 0
     );
-    processTransactions(relevantTransactions, computeUnitMap, addressToProgramsMap);
+    processTransactions(relevantTransactions, computeUnitMap, addressToProgramsMap, accountAddressToComputeMap);
 
     let payload : any = null;
+
+    const computeUnitsArray = Array.from(computeUnitMap).map(async ([programAddress, computeUnits]) => {
+        return {
+            programAddress,
+            programLabel: addressLabel(programAddress, Cluster.MainnetBeta) || programAddress,
+            computeUnits
+        }
+    });
+
+    const resolvedComputeUnitsArray = await Promise.all(computeUnitsArray);
+    
     if (computeUnitMap.size == 0) {
         return null;
     }
 
-    // convert the map into an array of objects
-    const computeUnitsArray = Array.from(computeUnitMap).map(async ([programAddress, computeUnits]) => {
-        const associatedAddresses = await findAssociatedAddresses(programAddress, relevantTransactions);
-        return {
-            programAddress,
-            programLabel: addressLabel(programAddress, Cluster.MainnetBeta) || programAddress,
-            computeUnits,
-            associatedAddresses,
-            computePerAddress : computeUnits / associatedAddresses.length
-        };
-    });
-    const resolvedComputeUnitsArray = await Promise.all(computeUnitsArray);
-    
-    //adding up all the compute units in the block. this will be one of the payload params.
-    let totalComputeUnitsMeta = 0;
-    let highestComputeUnit = 0;
-    for (const transaction of block.transactions) {
-        if (transaction.meta && transaction.meta.computeUnitsConsumed) {
-            highestComputeUnit = Math.max(highestComputeUnit, transaction.meta.computeUnitsConsumed);
-            totalComputeUnitsMeta += transaction.meta.computeUnitsConsumed;
-        }
-    }
-
-    // Convert addressToProgramsMap to array
-    const addressToProgramsArray = Array.from(addressToProgramsMap).map(([address, associatedPrograms]) => {
-        return {
-            address,
-            associatedPrograms: Array.from(associatedPrograms)
-        };
-    });
-
+    //create informativeAccounts array
     let informativeAccounts : InformativeAccount[] = [];
-    //for each in addressToProgramsArray, build informative object
-    for (let i = 0; i < addressToProgramsArray.length; i++) {
-        const addressToPrograms = addressToProgramsArray[i];
-        const address = addressToPrograms.address;
-        const associatedPrograms = addressToPrograms.associatedPrograms;
-
-        // get CU sum from addressToProgramsArray
-        const computeUnits = addressToProgramsArray[i].associatedPrograms.reduce((acc, program) => {
-            return acc + program.compute;
-        }, 0);
-
-        //create a version of associatedPrograms that just has the program addresses
-        const associatedProgramsAddresses = associatedPrograms.map(program => program.programAddress);
+    //for each in addressToProgramsMap, build informative object
+    addressToProgramsMap.forEach((associatedPrograms, address) => {
         const currentInformativeAccount : InformativeAccount = {
             address : address,
             addressLabel: addressLabel(address, Cluster.MainnetBeta) || address,
-            computeUnits: +computeUnits,
-            associatedPrograms: associatedProgramsAddresses
+            computeUnits: accountAddressToComputeMap.get(address) ?? 0,
+            associatedPrograms: Array.from(associatedPrograms)
         }
-
         informativeAccounts.push(currentInformativeAccount);
-
-    }
+    });
 
     if (config) {
         // Sort informativeAccounts array based on computeUnits in descending order
@@ -94,14 +62,19 @@ export async function processBlock(block: VersionedBlockResponse) {
         addressToLabelMap.set(obj.address, obj.addressLabel);
     }
 
+    const highestComputeUnit = Math.max(...Array.from(accountAddressToComputeMap.values()));
+
+    //payload must be an object with the following properties:
+    //informativeAccounts: array of account objects
+    //maxComputeUnits: number
+    //addressToLabelMap: map
+    //programsComputeUnits: array of objects with programAddress and computeUnits properties
     payload = JSON.stringify({
         slot: block.parentSlot + 1,
-        //computeUnitsMeta: totalComputeUnitsMeta,
-        programsComputeUnits: resolvedComputeUnitsArray, // holds type of progA, progL, compU, associatedA
-        //addressToPrograms: addressToProgramsArray,
         informativeAccounts: informativeAccounts,
         addressToLabelMap: Object.fromEntries(addressToLabelMap),
-        maxComputeUnits: highestComputeUnit
+        maxComputeUnits: highestComputeUnit,
+        programsComputeUnits: computeUnitsArray
     });
 
     return payload;
@@ -133,51 +106,85 @@ export async function findAssociatedAddresses(programAddress : string, targetTra
 
 
 
-export function processAccounts(accountIndices: any[], accountKeys: any, programAddress: string, computeUnitsConsumed: number, addressToProgramsMap: Map<string, Set<programWithCompute>>, payload: any) {
+export function processAccounts(accountIndices: any[], accountKeys: any, programAddress: string, computeUnitsConsumed: number, addressToProgramsMap: Map<string, Set<string>>, payload: any, programToAccounts: Map<string, string[]>) {
     let count = 0;
     accountIndices.forEach((index: any) => {
         if (!payload.isAccountWritable(index) || index >= accountKeys.length) return;
         count++;
         const address = accountKeys[index].toString();
-        const associatedPrograms = addressToProgramsMap.get(address) || new Set<programWithCompute>();
-        associatedPrograms.add({
-            programAddress,
-            compute: Math.floor(computeUnitsConsumed / count)
-        });
+
+        //add to programToAccounts
+        let associatedAccounts = programToAccounts.get(programAddress) || [];
+        associatedAccounts.push(address);
+        programToAccounts.set(programAddress, associatedAccounts);
+
+        const associatedPrograms = addressToProgramsMap.get(address) || new Set<string>();
+        associatedPrograms.add(programAddress);
         addressToProgramsMap.set(address, associatedPrograms);
     });
 }
 
-export function processTransactions(transactions: any[], computeUnitMap: Map<string, number>, addressToProgramsMap: Map<string, Set<programWithCompute>>) {
+export function processTransactions(transactions: any[], computeUnitMap: Map<string, number>, addressToProgramsMap: Map<string, Set<string>>, accountAddressToComputeMap: Map<string, number>) {
     if (!transactions || transactions.length === 0) {
         return;
     }
-    
+
     for (const transaction of transactions) {
+
+        //store relevant info from transaction
         const transactionType : TransactionType = transaction.version;
         const payload = transaction.transaction.message;
         const meta: ConfirmedTransactionMeta = transaction.meta as ConfirmedTransactionMeta ?? {};
         const logMessages = meta.logMessages;
-        if (!logMessages) continue;
+
+        if (!logMessages) continue; //next transaction if no log messages
+
+        //within each transaction, maintain programs' CU and accounts they touch
+        const localProgramToCU: Map<string, number> = new Map<string, number>();
+        const programToAccounts: Map<string, string[]> = new Map<string, string[]>();
 
         for (const logMessage of logMessages) {
             const match = logMessage.match(/Program (\S+) consumed (\d+) of \d+ compute units/);
-            if (!match) continue;
+            if (!match) continue; //next message if it tell us nothing
 
             const programAddress = match[1];
             const computeUnitsConsumed = parseInt(match[2], 10);
-            const currentComputeUnits = computeUnitMap.get(programAddress) || 0;
-            computeUnitMap.set(programAddress, currentComputeUnits + computeUnitsConsumed);
+
+            //add to localProgramToCU
+            const currentComputeUnits = localProgramToCU.get(programAddress) || 0;
+            localProgramToCU.set(programAddress, currentComputeUnits + computeUnitsConsumed);
+
+            // const currentComputeUnits = computeUnitMap.get(programAddress) || 0;
+            // computeUnitMap.set(programAddress, currentComputeUnits + computeUnitsConsumed);
 
             const instructions = transactionType === "legacy" ? payload.instructions : payload.compiledInstructions;
             const accountKeys = transactionType === "legacy" ? payload.accountKeys : payload.staticAccountKeys;
             if (instructions) {
                 instructions.forEach((instruction: any) => {
                     const accountIndices = transactionType === "legacy" ? instruction.accounts : instruction.accountKeyIndexes;
-                    processAccounts(accountIndices, accountKeys, programAddress, computeUnitsConsumed, addressToProgramsMap, payload);
+                    processAccounts(accountIndices, accountKeys, programAddress, computeUnitsConsumed, addressToProgramsMap, payload, programToAccounts);
                 });
             }
         }
+
+        //foreach program in localProgramToCU, add to computeUnitMap
+        localProgramToCU.forEach((computeUnits, programAddress) => {
+            const currentComputeUnits = computeUnitMap.get(programAddress) || 0;
+            computeUnitMap.set(programAddress, currentComputeUnits + computeUnits);
+        }
+        );
+
+        //foreach program in programToAccounts, access localProgramToCU divide by number of accounts and for each account add to addressToProgramsMap
+        programToAccounts.forEach((associatedAccounts, programAddress) => {
+            const computeUnits = localProgramToCU.get(programAddress) || 0;
+            const computePerAccount = Math.floor(computeUnits / associatedAccounts.length);
+            associatedAccounts.forEach((accountAddress) => {
+                const currentComputeUnits = accountAddressToComputeMap.get(accountAddress) || 0;
+                accountAddressToComputeMap.set(accountAddress, currentComputeUnits + computePerAccount);
+            });
+        }
+        );
+
     }
 }
 
