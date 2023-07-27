8fc06a6c0385b347af1e720dbf2571a8bbb5e29f
Alan Wu <alanwu100@gmail.com>
2023-07-27 15:53:42 -0500

    refactor: cleanup

--- public/visualization.js
+++ public/visualization.js
@@ -1,12 +1,10 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import { computeSha256, scaleHashToNumber, 
     createTooltip, hideTooltip, showTooltip, createScales, removeStrayTooltips } from "./utils.js";    
-import { config } from "./config.js";
 import { AccountNodev2, ProgramNodev2, Line } from "./nodes.js";
 
 let accountState = {};
 let programState = {};
-// let lineState  = [];
 let lineState = {};
 
 /*
@@ -94,7 +92,7 @@ export async function draw(data) {
             programState[program.programAddress] = new ProgramNodev2(hash, program.programAddress, originalAddressLabelMap.get(program.programAddress), null, program.computeUnits, lightnessScale, hueScale, null);
         }
     }
-    
+
     Object.values(lineState).forEach(line => {
         //update the references to the account and program nodes
         line.account = accountState[line.account.address];
@@ -106,46 +104,7 @@ export async function draw(data) {
             console.log(`deleting line ${line.key}`);
             delete lineState[line.key];
         }
-        /*
-        if (line.checkDeath()) {
-            console.log("Deleting line");
-            //check if it was the account or program that died
-            if ( !(line.program.address in programState) ) {
-                // program died
-                console.log("Program died");
-                //check if the account has any other lines
-                let accountHasOtherLines = false;
-                Object.values(lineState).forEach(otherLine => {
-                    if (otherLine.account.address === line.account.address) {
-                        accountHasOtherLines = true;
-                    }
-                });
-                if (!accountHasOtherLines) {
-                    // account has no other lines, delete it too
-                    console.log("Account died");
-                    accountState.splice(accountState.indexOf(line.account), 1);
-                }
-            } else {
-                // account died
-                console.log("Account died");
-                //check if the program has any other lines
-                let programHasOtherLines = false;
-                Object.values(lineState).forEach(otherLine => {
-                    if (otherLine.program.address === line.program.address) {
-                        programHasOtherLines = true;
-                    }
-                });
-                if (!programHasOtherLines) {
-                    // program has no other lines, delete it too
-                    console.log("Program died");
-                    programState.splice(programState.indexOf(line.program), 1);
-                }
-            }
-            lineState.delete(line.key);
-        }
-        */
     });
-
 
     // Update or make new lines
     for (const account of accounts) {
@@ -192,103 +151,10 @@ export async function draw(data) {
 
     Object.values(programState).forEach(node => {
         const { x, y } = node.position;
-        drawProgram(svg, x, y, 6, node.fill, node.tooltipContent, tooltip);
+        const latest = node.fadedness == 0;
+        drawProgram(svg, x, y, 6, node.fill, node.tooltipContent, tooltip, latest);
         //    .style("opacity", node.alpha);
     });
-
-    
-
-    /*
-    if (!accounts) return;
-    for (const account of accounts) {
-        const address = account.address;
-        const hash = await computeSha256(address);
-
-        if (address in accountState) {
-            // Reset fadedness if node already exists in state
-            accountState[address].resetFadedness(account);
-            // Update compute units
-            accountState[address].updateComputeUnits(account.computeUnits);
-            // Update Programs
-            for (const program of account.associatedPrograms) {
-                if (program in programState) {
-                    programState[program].updateAssociations(address);
-                } else {
-                    const hash = await computeSha256(program);
-                    //find compute units of program
-                    let computeUnits = programComputeUnitsMap.get(program);
-                    programState[program] = new ProgramNodev2(hash, program, originalAddressLabelMap.get(program), [address], computeUnits, lightnessScale, hueScale);
-                }
-            }
-        } else {
-            // Add new node to state
-            accountState[address] = new AccountNodev2(hash, address, account.addressLabel, account.associatedPrograms, account.computeUnits, lightnessScale, hueScale, opacityScale);
-        }
-    }
-
-    // Make sure to remove all program nodes that aren't associated with alive accounts
-    //for each value in the map, run scanAssociations and then check if referenceCount == 0, if so, delete
-    Object.keys(programState).forEach(program => {
-        const node = programState[program];
-        node.scanAssociations(accountState);
-        if (node.getReferenceCount() == 0) {
-            delete programState[program];
-        }
-    });
-
-    // clear the svg
-    console.log("clearing");
-    svg.selectAll("*").remove();
-    let lines = svg.append("g").attr("id", "linesGroup"); // add id for clarity
-
-    // Get rid of bad lines
-    lineState = lineState.filter(line => !line.checkDeath());
-
-    let lineKeysSet = new Set(lineState.map(line => line.key));
-    // Add valid lines
-    for (let program in programState) {
-        const node = programState[program];
-        for (let account of node.associatedAccounts) {
-            // Create a key that uniquely identifies this line
-            const lineKey = `${program}-${account}`;
-
-            if (!lineKeysSet.has(lineKey)) {
-                lineState.push(new Line(node, accountState[account], lineKey, programState, accountState));
-                lineKeysSet.add(lineKey);
-            }            
-        }
-    }
-    console.log(lineState.length);
-
-    //Draw lines
-    for (let line of lineState) {
-        const { x: x1, y: y1 } = line.programPosition;
-        const { x: x2, y: y2 } = line.accountPosition;
-        // console.log(`drawing with alpha ${line.program.alpha} and ${line.account.alpha}`);
-        drawLine(lines, x1, y1, x2, y2, line.program.alpha, line.account.alpha);
-    }
-
-    // Draw nodes
-
-    //draw program nodes
-    for (let program in programState) {
-        const node = programState[program];
-        const { x, y } = node.position;
-        drawProgram(svg, x, y, 6, node.fill, node.tooltipContent, tooltip)
-            .style("opacity", programState[program].alpha);
-    }
-
-    //draw account nodes
-    for (let address in accountState) {
-        const node = accountState[address];
-        const { x, y } = node.position;
-        // draw each
-        drawAccount(svg, x, y, 4, node.fill, node.tooltipContent, tooltip)
-            .style("opacity", accountState[address].alpha);
-        node.drawBackgroundShape(svg);
-    }
-
-    */
 
     removeStrayTooltips();
 
@@ -296,8 +162,12 @@ export async function draw(data) {
 
 
 
+let delay = 40;
+let duration = 400;
+let easing = d3.easeCubic;
+let scaleIncrease = 1.5;
 
-function drawProgram(svg, cx, cy, radius, fill, content, tooltip) {
+function drawProgram(svg, cx, cy, radius, fill, content, tooltip, latest) {
     const circle = svg.append("circle")
         .attr("cx", cx)
         .attr("cy", cy)
@@ -307,17 +177,18 @@ function drawProgram(svg, cx, cy, radius, fill, content, tooltip) {
         .on("mouseout", () => hideTooltip(tooltip));
 
     // Pulse animation to indicate the circle was in the most recent block
-    /*
-    circle.transition()
-        .duration(100)
-        .ease(d3.easeElastic)
-        .attr("r", radius * 1.3)
+    if (latest) {
+        circle.transition()
+        .delay(delay)
+        .duration(duration)
+        .ease(easing)
+        .attr("r", radius * scaleIncrease)
         .transition()
-        .duration(100)
-        .ease(d3.easeElastic)
+        .duration(duration)
+        .ease(easing)
         .attr("r", radius);
-    */
-
+    }
+    
     return circle;
 }
 
