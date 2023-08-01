bae0e05eb887d407963cf097f2cbe93cb4541dc0
Alan Wu <60207036+alanhwu@users.noreply.github.com>
2023-08-01 14:09:33 -0500

    feat: filter based on token or program interaction (#14)
    * feat: target dropdown menu
    
    * feat: filter visualization by token
    
    * feat: filter by program

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 import { draw } from './visualization.js';
 import { reformatString } from './utils.js';
+import { config } from './config.js';
 const websocket = new WebSocket('ws://localhost:3000');
 
 const slider = document.getElementById('slider');
@@ -30,6 +31,122 @@ toggle1.onchange = function() {
     animationBool = this.checked;
 }
 
+let selectedItem = null;
+let targetAddress = null;
+let targetType = null;
+
+const configItems = config.topItems;
+const topItems =
+    configItems.map(item => {
+        const { address, ...rest } = item;
+        return rest;
+    }); 
+  
+// Add a click event listener to show all items when the input box is clicked
+document.querySelector('#searchInput').addEventListener('click', function() {
+    const dropdownItems = document.querySelector('#dropdownItems');
+    dropdownItems.innerHTML = '';
+  
+    topItems.forEach(item => {
+      const div = document.createElement('div');
+      div.textContent = item.name;
+      if (item.type === 'category') {
+        div.classList.add('dropdownCategory');
+        const img = document.createElement('img');
+        img.src = 'arrow.png'; // path to the icon image
+        img.style.width = '16px'; // set width of the image
+        img.style.height = '12px'; // set height of the image
+        div.prepend(img);
+      }
+      dropdownItems.appendChild(div);
+    });
+  
+    dropdownItems.style.display = 'block';
+  });
+  
+  // Adjust the input event listener to filter items only when something is typed
+  document.querySelector('#searchInput').addEventListener('input', function(e) {
+    const dropdownItems = document.querySelector('#dropdownItems');
+    dropdownItems.innerHTML = '';
+  
+    if (e.target.value) {
+      const filteredItems = topItems.filter(item =>
+        item.type !== 'category' && item.name.toLowerCase().startsWith(e.target.value.toLowerCase())
+      );
+  
+      filteredItems.forEach(item => {
+        const div = document.createElement('div');
+        div.textContent = item.name;
+        if (item.type === 'category') {
+          div.classList.add('dropdownCategory');
+          const img = document.createElement('img');
+          img.src = 'arrow.png'; // path to the icon image
+          img.style.width = '16px'; // set width of the image
+          img.style.height = '12px'; // set height of the image
+          div.prepend(img);
+        }
+        dropdownItems.appendChild(div);
+      });
+    } else {
+      // Display all items when the input is empty
+      topItems.forEach(item => {
+        const div = document.createElement('div');
+        div.textContent = item.name;
+        if (item.type === 'category') {
+          div.classList.add('dropdownCategory');
+          const img = document.createElement('img');
+          img.src = 'arrow.png'; // path to the icon image
+          img.style.width = '16px'; // set width of the image
+          img.style.height = '12px'; // set height of the image
+          div.prepend(img);
+        }
+        dropdownItems.appendChild(div);
+      });
+    }
+  
+  });
+  
+  document.querySelector('#dropdownItems').addEventListener('click', function(e) {
+    console.log(`dropdownItems clicked`);
+    if (e.target.tagName === 'DIV' && !e.target.classList.contains('dropdownCategory')) {
+        // Update the input field with the selected item's name
+        document.querySelector('#searchInput').value = e.target.textContent;
+
+        // Set the selected item variable
+        selectedItem = e.target.textContent;
+        console.log(selectedItem);
+        targetAddress = configItems.find(item => item.name === selectedItem).address;
+        targetType = configItems.find(item => item.name === selectedItem).form;
+        console.log(`targetAddress: ${targetAddress}`);
+        console.log(`targetType: ${targetType}`);
+
+        dropdownItems.style.display = 'none';
+    }
+});
+  
+// Add a blur event listener to hide the items when the input box loses focus
+document.querySelector('#searchInput').addEventListener('blur', function() {
+    // Use setTimeout to delay the hiding of the dropdown
+    setTimeout(function() {
+      document.querySelector('#dropdownItems').style.display = 'none';
+    }, 100); // Delay time in milliseconds
+  });
+
+  // Add a click event listener to the clear button
+document.querySelector('#clearInput').addEventListener('click', function() {
+  // Clear the input field
+  document.querySelector('#searchInput').value = '';
+
+  // Reset the selectedItem variable
+  selectedItem = null;
+  targetAddress = null;
+  targetType = null;
+  console.log('cleared the target address');
+  console.log(`targetAddress: ${targetAddress}`);
+  console.log(`selectedItem: ${selectedItem}`);
+});
+
+  
 
 
 
@@ -76,12 +193,62 @@ async function processQueue() {
     while (true) {
         if (stateQueue.length > 0) {
             console.log(`length of stateQueue: ${stateQueue.length}`);
-            const firstElement = stateQueue.shift();
+            let firstElement = stateQueue.shift();
+            console.log(`the target address is: ${targetAddress}`);
+            if (targetAddress) {
+              console.log('target address detected');
+              if (targetType == 'program') {
+                let relevantPrograms = new Set();
+                console.log('filter based on program');
+                const filteredItems = firstElement.informativeAccounts.filter(account => {
+                  if (account.associatedPrograms.includes(targetAddress)) {
+                    account.associatedPrograms.forEach(program => {
+                      relevantPrograms.add(program);
+                    });
+                    return true;
+                  }
+                  return false;
+                });
+
+                const filteredPrograms = firstElement.programsComputeUnits.filter(program => {
+                  // Return true if the program.programAddress is in relevantPrograms
+                  return relevantPrograms.has(program.programAddress);
+                });
+                firstElement.informativeAccounts = filteredItems;
+                firstElement.programsComputeUnits = filteredPrograms;
+              }
+              else if (targetType == 'token') {
+                console.log('filter based on token');
+                // Filter the firstElement's informativeAccounts to only include accounts that have the token in its tokenTags
+                let relevantPrograms = new Set();
+            
+                const filteredItems = firstElement.informativeAccounts.filter(account => {
+                    if (account.tokenTags.includes(targetAddress)) {
+                        account.associatedPrograms.forEach(program => {
+                            relevantPrograms.add(program);
+                        });
+                        return true;
+                    }
+                    return false;
+                });
+            
+                const filteredPrograms = firstElement.programsComputeUnits.filter(program => {
+                    // Return true if the program.programAddress is in relevantPrograms
+                    return relevantPrograms.has(program.programAddress);
+                });
+            
+                firstElement.informativeAccounts = filteredItems;
+                firstElement.programsComputeUnits = filteredPrograms;
+              }
+            }
+              
             await draw(firstElement, maxAccounts, animationBool, history);
+  
         }
         await new Promise(r => setTimeout(r, 1000));
     }
 }
+
 
 
 
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const config = {
+    trimAccounts: false,
     topAccountsCount: 100,
     svgLength: 1150,
     svgHeight: 650,
