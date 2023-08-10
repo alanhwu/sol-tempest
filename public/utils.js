b92e82e03a69332cc614a05bec6ecc2caeab6f00
Alan Wu <alanwu100@gmail.com>
2023-08-09 18:08:14 -0700

    build: new sha256 library

--- public/utils.js
+++ public/utils.js
@@ -1,12 +1,9 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-export async function computeSha256(input) {
-    const buffer = new TextEncoder().encode(input);
-    const digest = await crypto.subtle.digest('SHA-256', buffer);
-    return Array.from(new Uint8Array(digest))
-        .map(byte => byte.toString(16).padStart(2, '0'))
-        .join('');
-}
+export function computeSha256(input) {
+    const hash = SHA256.createHash().update(new TextEncoder().encode(input)).digest("hex");
+    return hash;
+  }
 
 export function scaleHashToNumber(hash, min, max) {
     const hashInt = parseInt(hash.slice(0, 8), 16);
